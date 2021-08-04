1dd352ac9907295da21d21a82defb36912f81c3c
Sia Karamalegos <siakaramalegos@gmail.com>
2021-08-04 17:12:46 -0500

    5 colors with input changes color

--- src/components/ColorComponent.js
+++ src/components/ColorComponent.js
@@ -3,19 +3,31 @@ import { useState } from "preact/hooks";
 import { hexToHsluv } from "hsluv";
 import ColorBlock from "./ColorBlock";
 import ColorGradations from "./ColorGradations";
+import tinycolor from "tinycolor2";
 
-const ColorComponent = () => {
-  const [color, setColor] = useState("#e61919");
+// defaultColor needs to be a hex but inputted color can be any valid CSS color
+const ColorComponent = ({ defaultColor = "#e61919" }) => {
+  const [color, setColor] = useState(defaultColor);
+  const changeColor = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const inputtedColor = form.get("color");
+
+    const hex = tinycolor(inputtedColor).toHex();
+    setColor(`#${hex}`);
+  };
 
   const hsluvColor = hexToHsluv(color);
-  console.log({ hsluvColor });
 
   return (
     <div className="color-grid">
-      <label>
-        CSS color value
-        <input value={color} onInput={(e) => setColor(e.target.value)} />
-      </label>
+      <form onSubmit={changeColor}>
+        <label>
+          CSS color value
+          <input name="color" value={color} />
+        </label>
+        <button>Generate scale</button>
+      </form>
       <ColorBlock color={color} width={"100%"} />
       <ColorGradations hue={hsluvColor[0]} saturation={hsluvColor[1]} />
     </div>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,7 +5,11 @@ import style from "./style.css";
 const Home = () => (
   <div class={style.home}>
     <h1>Web Palette Generator</h1>
-    <ColorComponent />
+    <ColorComponent defaultColor={"#e6b919"} />
+    <ColorComponent defaultColor={"#663399"} />
+    <ColorComponent defaultColor={"#19e60a"} />
+    <ColorComponent defaultColor={"#FFFFFF"} />
+    <ColorComponent defaultColor={"#000000"} />
   </div>
 );
 
